7fbf3475d7b9ec418bd5979e3cdb4dcd8d9914ea
Matthew Levene <matthew.levene@hotmail.co.uk>
2021-03-11 14:44:59 +0000

    Implement network call to Unsplash backend

--- app/src/main/java/com/ml/photosearchapp/network/PhotoApi.kt
+++ app/src/main/java/com/ml/photosearchapp/network/PhotoApi.kt
@@ -1,8 +1,22 @@
 package com.ml.photosearchapp.network
 
+import com.ml.photosearchapp.BuildConfig
+import com.ml.photosearchapp.domain.UnsplashResponse
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
 interface PhotoApi {
 
     companion object {
         const val BASE_URL = "https://api.unsplash.com/"
     }
+
+    @Headers("Authorization: Client-ID ${BuildConfig.ACCESS_KEY}")
+    @GET("search/photos")
+    fun getPhotos(
+            @Query("query") query: String,
+            @Query("page") page: Int,
+            @Query("per_page") perPage: Int
+    ) : UnsplashResponse
 }
