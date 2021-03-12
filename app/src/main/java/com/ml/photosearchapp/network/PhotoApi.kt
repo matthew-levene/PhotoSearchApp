8be41aa692de3041b793457859e39c7596e7c2a5
Matthew Levene <matthew.levene@hotmail.co.uk>
2021-03-12 16:09:16 +0000

    Implement network call to user photos endpoint

--- app/src/main/java/com/ml/photosearchapp/network/PhotoApi.kt
+++ app/src/main/java/com/ml/photosearchapp/network/PhotoApi.kt
@@ -2,8 +2,10 @@ package com.ml.photosearchapp.network
 
 import com.ml.photosearchapp.BuildConfig
 import com.ml.photosearchapp.domain.UnsplashResponse
+import com.ml.photosearchapp.domain.authored.AuthoredPhotoResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PhotoApi {
@@ -19,4 +21,13 @@ interface PhotoApi {
             @Query("page") page: Int,
             @Query("per_page") perPage: Int
     ) : UnsplashResponse
+
+    @Headers("Authorization: Client-ID ${BuildConfig.ACCESS_KEY}")
+    @GET("users/{username}/photos")
+    suspend fun getAuthoredPhotos(
+        @Path("username") username: String,
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+    ) : List<AuthoredPhotoResponse>
+
 }
