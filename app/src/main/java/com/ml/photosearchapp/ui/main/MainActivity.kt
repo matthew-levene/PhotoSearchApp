50e386f2a16c48d0f0eb4ccb94750328f20f78a5
Matthew Levene <matthew.levene@hotmail.co.uk>
2021-03-12 14:41:55 +0000

    Add navigation to action bar and transition animations

--- app/src/main/java/com/ml/photosearchapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/ml/photosearchapp/ui/main/MainActivity.kt
@@ -2,6 +2,12 @@ package com.ml.photosearchapp.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.ml.photosearchapp.BuildConfig
 import com.ml.photosearchapp.R
 import dagger.hilt.EntryPoint
@@ -9,9 +15,25 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupAppbar()
     }
 
+    override fun onSupportNavigateUp(): Boolean =
+        navController.navigateUp()
+
+    private fun setupAppbar() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+
+        navController = navHostFragment.findNavController()
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
 }
