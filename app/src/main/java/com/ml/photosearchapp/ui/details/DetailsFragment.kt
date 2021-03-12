7a0d735a3f3984cc9dfbcf0826cbd726eab6153d
Matthew Levene <matthew.levene@hotmail.co.uk>
2021-03-12 16:11:44 +0000

    Connect details fragment to paging data source and UI adapter

--- app/src/main/java/com/ml/photosearchapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/ml/photosearchapp/ui/details/DetailsFragment.kt
@@ -5,22 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.ml.photosearchapp.R
 import com.ml.photosearchapp.databinding.FragmentDetailsBinding
+import com.ml.photosearchapp.ui.details.adapter.DetailsAdapter
+import com.ml.photosearchapp.ui.details.viewmodel.DetailsViewModel
+import com.ml.photosearchapp.ui.gallery.adapter.ReloadErrorAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.fragment_details) {
 
-  //  private val galleryViewModel by viewModels<GalleryViewModel>()
+    private val detailsViewModel by viewModels<DetailsViewModel>()
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding: FragmentDetailsBinding get() = _binding ?: throw(IllegalAccessException())
 
     private val args by navArgs<DetailsFragmentArgs>()
+
+    private lateinit var detailsAdapter: DetailsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +45,7 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
         setupViews()
         setupRecyclerView()
         setupObserver()
+        detailsViewModel.searchPhotosByAuthor(args.results.user.username)
     }
 
     override fun onDestroyView() {
@@ -59,10 +69,19 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
     }
 
     private fun setupRecyclerView() {
-
+        detailsAdapter = DetailsAdapter()
+        binding.detailsRecyclerview.adapter = detailsAdapter
+            .withLoadStateHeaderAndFooter(
+                ReloadErrorAdapter { detailsAdapter.retry() },
+                ReloadErrorAdapter { detailsAdapter.retry() }
+            )
     }
 
     private fun setupObserver() {
-
+        detailsViewModel.photos.observe(viewLifecycleOwner) {
+           lifecycleScope.launch {
+               detailsAdapter.submitData(it)
+           }
+        }
     }
 }
